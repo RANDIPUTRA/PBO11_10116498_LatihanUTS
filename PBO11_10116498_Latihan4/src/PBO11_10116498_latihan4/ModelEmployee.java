e23649b2a66a916fdf2b0466187066112bc64369
RANDIPUTRA <32483773+RANDIPUTRA@users.noreply.github.com>
2017-11-14 21:49:12 +0700

    Add files via upload

---
+++ PBO11_10116498_Latihan4/src/PBO11_10116498_latihan4/ModelEmployee.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO11_10116498_latihan4;
+
+/**
+ *
+ * @author randi putra
+ */
+public class ModelEmployee {
+    
+   float salary =  40000;
+    
+}
---
+++ PBO11_10116498_Latihan5/src/PBO11_10116498_latihan5/BangunDatar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO11_10116498_latihan5;
+
+/**
+ *
+ * @author randi putra
+ */
+public interface BangunDatar {
+    
+    public void hitungluas();
+    public void hitungkeliling();
+    
+}
